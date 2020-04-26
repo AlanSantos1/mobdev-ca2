9ab301b6f825335e30a8725d7504f134655434ea
AlanSantos1 <alan.informas@gmail.com>
2020-04-26 13:32:22 +0000

    quotes

--- mobdev-ca2/src/app/pages/quotes/quotes.page.ts
+++ mobdev-ca2/src/app/pages/quotes/quotes.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { NavController } from '@ionic/angular';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-quotes',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesPage implements OnInit {
 
-  constructor() { }
+    quotes: Observable<any>;
+
+  constructor(private router: Router,private api:ApiService) { }
 
   ngOnInit() {
+      this.quotes = this.api.getQuotes();
+  this.quotes.subscribe(data => {
+      console.log('my quote: ', data);
+    });
   }
-
+  openDetails(quote){
+      let quoteId = quote.quote_id
+      this.router.navigateByUrl(`/tabs/quotes/${quoteId}`);
+  }
 }
